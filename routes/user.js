a4e0418b997403226648239cfb60ca5ab22726cf
gibson-lee93 <gb.lee.gibson@gmail.com>
2021-04-30 11:46:03 +0900

    Added POST /user/login, GET /user/userinfo in router

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const { userController } = require('../controller');
+
+// * POST /user/login
+router.post('/login', userController.login.post);
+
+// * GET /user/userinfo
+router.get('/userinfo', userController.userinfo.get);
 
 module.exports = router;
