539f546df2e765fe723f67265626b1571b90b421
gibson-lee93 <gb.lee.gibson@gmail.com>
2021-04-30 13:54:09 +0900

    Added Post /user/logout, /user/signup

---
+++ controller/user/signup.js
@@ -0,0 +1,26 @@
+const { Users } = require('../../models');
+
+module.exports = {
+  post: (req, res) => {
+    const { password, user_name, email } = req.body;
+    Users.findOne({ where: { email } })
+    .then((data) => {
+      if(!data) {
+        Users.create({
+          user_name: user_name,
+          email: email,
+          password: password
+        })
+        .then((created) => {
+          return res.status(201).send('User Created');
+        })
+      }
+      else{
+        return res.status(409).send('User already exist');
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  }
+}
