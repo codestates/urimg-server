c4a4cc488347859689aade3cbc4fe27819de59ee
gibson-lee93 <gb.lee.gibson@gmail.com>
2021-05-01 21:11:10 +0900

    Edited cors origin in index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json());
 app.use(
   cors({
-    origin: false,
+    origin: true,
     credentials: true,
     methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
   })
