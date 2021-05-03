4e3486850bc0fb2590dbcc463250899c415afee1
gibson-lee93 <gb.lee.gibson@gmail.com>
2021-05-03 18:28:24 +0900

    Made POST /img/upload

--- routes/img.js
+++ routes/img.js
@@ -1,4 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+const{ imgController } = require('../controller');
+
+router.post('/upload', imgController.upload.post);
+
 module.exports = router;
